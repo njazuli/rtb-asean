a4ca2752a54fd3f20413d7f63abfdadf73c8b18e
njazuli <78890281+njazuli-deriv@users.noreply.github.com>
2021-07-03 23:23:49 +0800

    added Live page

--- components/PlayerDetails.js
+++ components/PlayerDetails.js
@@ -1,8 +1,22 @@
 import moment from "moment";
+import Head from "next/head";
 
 const PlayerDetails = ({ id, title, description, date }) => {
   return (
     <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d92d889385b2fe2"
+        ></script>
+        <title>ASEAN Summit 2021 {title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:image" content="../public/favicon.png" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <div className="w-100 my-2">
         <p className="main-title f_20 mb-0">{title}</p>
       </div>
@@ -16,12 +30,17 @@ const PlayerDetails = ({ id, title, description, date }) => {
                   mb-3
                   align-items-center
                   border-bottom
+                  flex-row
                 "
       >
-        <span className="f_14 date-time-and-share me-4">
-          {moment(date).format("D MMM YYYY")}
-        </span>
-        <span className="f_14 date-time-and-share">Share</span>
+        <div className="h-100 d-flex align-items-center">
+          <span className="f_14 date-time-and-share me-4">
+            {moment(date).format("D MMM YYYY")}
+          </span>
+        </div>
+        <div className="h-100 d-flex align-items-center">
+          <div class="addthis_inline_share_toolbox_gryg"></div>
+        </div>
       </div>
 
       <div className="w-100 mb-4">
--- pages/live.js
+++ pages/live.js
@@ -1,8 +1,9 @@
 import React, { createRef, useEffect, useState, useRef } from "react";
 import Head from "next/head";
+import LiveSection from "../components/LiveSection";
+import List from "../components/List";
 import Player from "../components/Player";
 import PlayerDetails from "../components/PlayerDetails";
-import List from "../components/List";
 import axios from "axios";
 
 export async function getStaticProps() {
@@ -21,7 +22,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ data }) {
+export default function Live({ data }) {
   const is_data = data.data;
   const is_first_list = data.data.data["program-episode"];
   const [is_list, setList] = useState([]);
@@ -31,9 +32,7 @@ export default function Home({ data }) {
   const [is_title, setTitle] = useState();
   const [is_description, setDescription] = useState();
   const [is_date, setDate] = useState();
-  let widget_timeout = null;
-  let script_timeout = null;
-  const widgetEl = createRef(null);
+  const live_player_id = 2;
 
   useEffect(() => {
     getEachData(is_first_list);
@@ -45,31 +44,6 @@ export default function Home({ data }) {
     setDate(is_item_details.dateCreated);
     setPlayerId(is_item_details.id);
   }, [is_item_details]);
-
-  // load widget
-  useEffect(() => {
-    const containerRef = widgetEl.current;
-    let widget = null;
-    let widgetresizer = null;
-
-    widget_timeout = setTimeout(() => {
-      widget = new RTBCountdownWidget(
-        "https://njazuli.github.io/rtb-asean-widget/"
-      );
-      widget.loadBanner(containerRef);
-    }, 1800);
-    script_timeout = setTimeout(() => {
-      widgetresizer = iFrameResize({
-        log: false,
-        inPageLinks: true,
-      });
-    }, 2300);
-
-    return () => {
-      clearTimeout(script_timeout);
-      clearTimeout(widget_timeout);
-    };
-  }, []);
 
   const getEachData = (list) => {
     Promise.all(
@@ -101,6 +75,7 @@ export default function Home({ data }) {
         <script src="https://njazuli.github.io/rtb-asean-widget/banner/js/iframeResizer.min.js"></script>
       </Head>
       <div className="w-100 main-top-pattern"></div>
+      <LiveSection liveplayerId={live_player_id} />
       <section className="main-padding">
         <div className="container">
           <div className="row mb-2">
@@ -117,9 +92,6 @@ export default function Home({ data }) {
                 description={is_description}
                 date={is_date}
               />
-              <div className="w-100 mb-3 mb-lg-0">
-                <div ref={widgetEl} id="widgetBanner"></div>
-              </div>
             </div>
             <div className="col-12 col-lg-4">
               <p className="fw-bold f_22">More Videos</p>
