08a1219a0e14586c1a7dc56a3a38242a550bba2b
njazuli <78890281+njazuli-deriv@users.noreply.github.com>
2021-08-05 00:09:29 +0800

    added revalidation for the file

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export async function getStaticProps() {
   }
   return {
     props: { data }, // will be passed to the page component as props
+    revalidate: 120, // Revalidate every 10 seconds with new data.
   };
 }
 
