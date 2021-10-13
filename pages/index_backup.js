c7a238b564fa4767e2a167dd6248cf586808a660
njazuli <78890281+njazuli-deriv@users.noreply.github.com>
2021-10-13 14:02:36 +0800

    replace live tv page on index page

--- pages/index_backup.js
+++ pages/index_backup.js
@@ -2,8 +2,25 @@ import React, { createRef, useEffect, useState } from "react";
 import Head from "next/head";
 import Player from "../components/Player";
 import PlayerDetails from "../components/PlayerDetails";
-import LiveSection from "../components/LiveSection";
 import List from "../components/List";
+import axios from "axios";
+
+// export async function getStaticProps() {
+//   const res = await fetch(
+//     `https://rtb.glueapi.io/v1/content?idParent=3147&sort=-data.episode_number/ns`
+//   );
+//   const data = await res.json();
+
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//     revalidate: 10, // Revalidate every 10 seconds with new data.
+//   };
+// }
 
 export default function Home() {
   const [isMounted, setMounted] = useState(false);
@@ -14,7 +31,9 @@ export default function Home() {
   const [is_title, setTitle] = useState();
   const [is_description, setDescription] = useState();
   const [is_date, setDate] = useState();
-  const live_player_id = 2;
+  let widget_timeout = null;
+  let script_timeout = null;
+  const widgetEl = createRef(null);
 
   async function getItemsData() {
     const res = await fetch(
@@ -52,15 +71,39 @@ export default function Home() {
     setPlayerId(is_item_details.id);
   }, [is_item_details]);
 
+  // load widget
+  useEffect(() => {
+    const containerRef = widgetEl.current;
+    let widget = null;
+    let widgetresizer = null;
+
+    widget_timeout = setTimeout(() => {
+      widget = new RTBCountdownWidget("https://widgetasean.ipsb.com.my/");
+      widget.loadBanner(containerRef);
+    }, 1800);
+    script_timeout = setTimeout(() => {
+      widgetresizer = iFrameResize({
+        log: false,
+        inPageLinks: true,
+      });
+    }, 2300);
+
+    return () => {
+      clearTimeout(script_timeout);
+      clearTimeout(widget_timeout);
+    };
+  }, []);
+
   return (
     <div className="bg_pattern">
       <Head>
         <title>ASEAN Summit 2021</title>
         <meta name="description" content="ASEAN Summit 2021" />
         <link rel="icon" href="/static/favicon.ico" />
+        <script src="https://widgetasean.ipsb.com.my/rtb-countdown-widget.js?nocache"></script>
+        <script src="https://widgetasean.ipsb.com.my/banner/js/iframeResizer.min.js"></script>
       </Head>
       <div className="w-100 main-top-pattern"></div>
-      <LiveSection liveplayerId={live_player_id} />
       <section className="main-padding">
         <div className="container">
           <div className="row mb-2">
@@ -77,6 +120,9 @@ export default function Home() {
                 description={is_description}
                 date={is_date}
               />
+              <div className="w-100 mb-3 mb-lg-0">
+                <div ref={widgetEl} id="widgetBanner"></div>
+              </div>
             </div>
             <div className="col-12 col-lg-5 col-xl-4">
               <p className="fw-bold f_22">More Videos</p>
